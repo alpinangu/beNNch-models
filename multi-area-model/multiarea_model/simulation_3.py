7fbc9e7dfbc476c1553d68cfecd70b243e0a0ea2
alpinangu <inangualp@gmail.com>
2025-08-16 01:14:30 +0300

    conn_spec, no morph

--- multi-area-model/multiarea_model/simulation_3.py
+++ multi-area-model/multiarea_model/simulation_3.py
@@ -695,7 +695,7 @@ def connect(simulation,
                     mean_delay = network.params['delay_params']['delay_i']
                 delay_min = simulation.params['dt']
             else:
-                conn_spec['long_range'] = simulation.custom_params['morph']
+                #conn_spec['long_range'] = simulation.custom_params['morph']
                 w_min = 0.
                 w_max = np.inf
                 v = network.params['delay_params']['interarea_speed']
