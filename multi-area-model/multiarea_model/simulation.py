1dd6d3198318a91da6817f2fcd491cfdc2acc79f
alpinangu <inangualp@gmail.com>
2026-03-08 23:12:18 +0300

    remove sli mem, set to -1

--- multi-area-model/multiarea_model/simulation.py
+++ multi-area-model/multiarea_model/simulation.py
@@ -340,7 +340,7 @@ class Simulation:
         try:
             mem = nest.ll_api.sli_func('memory_thisjob')
         except AttributeError:
-            mem = nest.sli_func('memory_thisjob')
+            mem = -1 #nest.sli_func('memory_thisjob') sli is gone
         if isinstance(mem, dict):
             return mem['heap']
         else:
